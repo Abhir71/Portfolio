3081296453cc67ce8eff6aad0c6f49211e179d86
Abhir71 <122446734+Abhir71@users.noreply.github.com>
2024-06-25 06:37:15 +0530

    Add files via upload

---
+++ User_analytics.sql
@@ -0,0 +1,28 @@
+/*1. Users who have not posted any photos */
+select u.id, u.username
+from users u
+left join photos p on u.id = p.user_id
+where p.id is null;
+
+/*2 The photos that have received the most comments */
+select p.id, p.image_url, COUNT(c.id) AS comment_count
+from photos p
+join comments c ON p.id = c.photo_id
+group by  p.id, p.image_url
+order by  comment_count desc;
+
+/* 3. Most popular tags used in photos */
+select  t.id, t.tag_name, COUNT(pt.photo_id) AS usage_count
+from tags t
+join photo_tags pt on t.id = pt.tag_id
+group by t.id, t.tag_name
+order by  usage_count desc;
+
+/* Top Users who have posted most photos */
+select u.username , count(p.id) as photo_count
+from users u
+join photos p on u.id = p.user_id
+group by u.id
+order by photo_count desc
+limit 10;
+
